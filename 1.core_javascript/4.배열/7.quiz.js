e4e850ba23a7a8f43f7c0e9d88c496b8ff3dc093
jzinit <kyj32323@naver.com>
2024-03-25 17:26:08 +0900

    배열 실습 수정

--- 1.core_javascript/4.배열/7.quiz.js
+++ 1.core_javascript/4.배열/7.quiz.js
@@ -11,27 +11,24 @@
 4. 한 명의 멤버를 정확하게 수정할때까지 프로그램은 계속되어야 합니다.
 */
 
-var bigbang = ['탑', '지디', '태양', '대성','패배'];
+var tvxq = ['유노윤호', '최강창민', '영웅재중', '믹키유천', '시아준수'];
 
-var modifyName = prompt(`수정할 멤버 이름을 작성하쇼 `);
+while (true) {
+  var modifyTarget = prompt(`현재 멤버: [${tvxq}]\n수정할 이름을 입력하세요.`);
 
-while(true){
-  if(bigbang.includes(modifyName)){
-    var newName = prompt(`새로운 이름 작성 `);
-    // var index = bigbang.indexOf(modifyName) ;
-    // bigbang[index] = newName;
-    bigbang[bigbang.indexOf(modifyName)] = newName;
-    alert(`${modifyName}에서 ${newName}로 수정되었고 현재멤버는 ${bigbang}`);
+  var idx = tvxq.indexOf(modifyTarget);
+
+  if (idx !== -1) {
+    // 수정 타겟이 있는경우 -> 수정 진행
+    var newName = prompt('새로운 이름을 입력하세요!');
+    tvxq[idx] = newName;
+    //tvxq.splice(idx, 1, newName)/
+    alert(`수정 완료!\n남은 멤버: [${tvxq}]`);
     break;
-  }else{
-  
-    alert(`배열에 없는 이름임`);
+  } else {
+    // 없는 경우
+    alert(`${modifyTarget}은(는) 잘못된 이름입니다.\n다시 입력하세요!`);
   }
 }
-
-///var newName = prompt(`새로운 이름 작성 `);
-
-
-
 
 
